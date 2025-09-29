621b57abb83f43f4c43fa83802428a8d0917834c
Furqan Mirza <furqanmirza@Furqans-MacBook-Air.local>
2025-09-29 21:50:55 +0500

    provide solution to JoinTwoArrayById problem

--- src/json/JoinTwoArraysById.ts
+++ src/json/JoinTwoArraysById.ts
@@ -7,19 +7,32 @@ type JSONValue =
   | { [key: string]: JSONValue };
 type ArrayType = { id: number } & Record<string, JSONValue>;
 
+// function join(arr1: ArrayType[], arr2: ArrayType[]): ArrayType[] {
+//   const map = new Map<number, ArrayType>();
+
+//   arr1.forEach((item) => {
+//     map.set(item.id, item);
+//   });
+
+//   arr2.forEach((item) => {
+//     if (map.has(item.id)) {
+//       map.set(item.id, { ...map.get(item.id)!, ...item });
+//     } else {
+//       map.set(item.id, item);
+//     }
+//   });
+//   return Array.from(map.values()).sort((a, b) => a.id - b.id);
+// }
+
 function join(arr1: ArrayType[], arr2: ArrayType[]): ArrayType[] {
   const map = new Map<number, ArrayType>();
 
-  arr1.forEach((item) => {
-    map.set(item.id, item);
-  });
-
-  arr2.forEach((item) => {
+  arr1.concat(arr2).forEach((item) => {
     if (map.has(item.id)) {
-      map.set(item.id, { ...map.get(item.id)!, ...item});
+      Object.assign(map.get(item.id)!, item);
     } else {
       map.set(item.id, item);
     }
   });
-  return Array.from(map.values()).sort((a,b) => a.id - b.id)
+  return Array.from(map.values());
 }
