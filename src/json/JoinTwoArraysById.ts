4814734f9d5fd6fd482c5fd79788c5ae3a74678d
Furqan Mirza <furqanmirza@Furqans-MacBook-Air.local>
2025-09-29 21:30:45 +0500

    provide solutions to joinTwoArraysById and SortBy problems

---
+++ src/json/JoinTwoArraysById.ts
@@ -0,0 +1,25 @@
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+type ArrayType = { id: number } & Record<string, JSONValue>;
+
+function join(arr1: ArrayType[], arr2: ArrayType[]): ArrayType[] {
+  const map = new Map<number, ArrayType>();
+
+  arr1.forEach((item) => {
+    map.set(item.id, item);
+  });
+
+  arr2.forEach((item) => {
+    if (map.has(item.id)) {
+      map.set(item.id, { ...map.get(item.id)!, ...item});
+    } else {
+      map.set(item.id, item);
+    }
+  });
+  return Array.from(map.values()).sort((a,b) => a.id - b.id)
+}
