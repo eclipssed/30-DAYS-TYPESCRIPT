ccad0826ae84604df4fe2c3dd34e7074a8e7b153
Furqan Mirza <furqanmirza@Furqans-MacBook-Air.local>
2025-09-30 10:30:14 +0500

    provide solution to CompactObject problem

---
+++ src/json/CompactObject.ts
@@ -0,0 +1,24 @@
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | Array<JSONValue>;
+
+function compactObject(obj: Obj): Obj {
+  if (Array.isArray(obj)) {
+    return obj.filter(Boolean).map((item) => {
+      return typeof item === "object" ? compactObject(item as Obj) : item;
+    });
+  } else {
+    const result: Record<string, JSONValue> = {};
+    Object.entries(obj).forEach(([key, value]) => {
+      if (value) {
+        result[key] = typeof value === "object" ? compactObject(value) : value;
+      }
+    });
+    return result;
+  }
+}
